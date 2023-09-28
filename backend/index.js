3d2df728d87202df2b2354495af9b9da34bf7266
lorijseo <lorijseo@gmail.com>
2023-09-28 05:18:43 -0700

    allow origin login

--- backend/index.js
+++ backend/index.js
@@ -28,8 +28,8 @@ app.use(cookieParser())
 app.use(express.json())
 // app.use(cors())
 // app.use(cors({credentials: true, origin: process.env.CLIENT_URL}));
-// app.use(cors({credentials: true, origin: "https://box-server.vercel.app"}));
-app.use(cors({credentials: false}));
+app.use(cors({credentials: true, origin: "https://box-server.vercel.app/api/auth/login"}));
+
 
 
 
