724e2a31a10866c95fe97874a232904d995aca0e
lorijseo <lorijseo@gmail.com>
2023-09-28 05:00:30 -0700

    cors credential false

--- backend/index.js
+++ backend/index.js
@@ -26,9 +26,9 @@ if(process.env.NODE_ENV === 'development'){
 }
 app.use(cookieParser())
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 // app.use(cors({credentials: true, origin: process.env.CLIENT_URL}));
-app.use(cors({credentials: true, origin: "https://box-server.vercel.app"}));
+// app.use(cors({credentials: true, origin: "https://box-server.vercel.app"}));
 
 
 
